7ccb9ef23246379c5fb4788114b177235808edaf
yuanyuexiang <yuanyuexiang@126.com>
2021-11-08 12:22:58 +0800

    Update servertu.go

--- servertu.go
+++ servertu.go
@@ -56,14 +56,65 @@ SkipFrameError:
 
 // ListenRTU starts the Modbus server listening to a serial device.
 // For example:  err := s.ListenRTU(&serial.Config{Address: "/dev/ttyUSB0"})
-func (s *Server) ListenRTUX(serialConfig *serial.Config, report func(err error)) (err error) {
+func (s *Server) ListenRTUForLinux(serialConfig *serial.Config, report func(err error)) (err error) {
 	port, err := serial.Open(serialConfig)
 	if err != nil {
 		log.Printf("failed to open %s: %v\n", serialConfig.Address, err)
 		return
 	}
 	s.ports = append(s.ports, port)
-	go s.acceptSerialRequestsX(port, report)
+	go s.acceptSerialRequestsForLinux(port, report)
+	return err
+}
+
+func (s *Server) acceptSerialRequestsForLinux(port serial.Port, report func(err error)) {
+SkipFrameError:
+	for {
+		buffer := make([]byte, 512)
+
+		bytesRead, err := port.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("serial read error %v\n", err)
+			}
+			report(err)
+			return
+		}
+		log.Printf("bytesRead")
+		log.Println(bytesRead)
+
+		if bytesRead != 0 {
+
+			// Set the length of the packet to the number of read bytes.
+			packet := buffer[:bytesRead]
+
+			frame, err := NewRTUFrame(packet)
+			if err != nil {
+				log.Printf("bad serial frame error %v\n", err)
+				//The next line prevents RTU server from exiting when it receives a bad frame. Simply discard the erroneous
+				//frame and wait for next frame by jumping back to the beginning of the 'for' loop.
+				log.Printf("Keep the RTU server running!!\n")
+				continue SkipFrameError
+				//return
+			}
+
+			request := &Request{port, frame}
+
+			s.requestChan <- request
+		}
+	}
+}
+
+// ListenRTU starts the Modbus server listening to a serial device.
+// For example:  err := s.ListenRTU(&serial.Config{Address: "/dev/ttyUSB0"})
+func (s *Server) ListenRTUForWindows(serialConfig *serial.Config, report func(err error)) (err error) {
+	port, err := serial.Open(serialConfig)
+	if err != nil {
+		log.Printf("failed to open %s: %v\n", serialConfig.Address, err)
+		return
+	}
+	s.ports = append(s.ports, port)
+	go s.acceptSerialRequestsForWindows(port, report)
 	return err
 }
 
@@ -72,7 +123,7 @@ var (
 	total = 0
 )
 
-func (s *Server) acceptSerialRequestsX(port serial.Port, report func(err error)) {
+func (s *Server) acceptSerialRequestsForWindows(port serial.Port, report func(err error)) {
 SkipFrameError:
 	for {
 		buffer := make([]byte, 512)
