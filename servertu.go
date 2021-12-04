8ebceee9dfaa112fc91f66f883f5cb2fd3a88c8a
yuanyuexiang <yuanyuexiang@126.com>
2021-12-04 15:50:36 +0800

    Update servertu.go

--- servertu.go
+++ servertu.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"log"
 
+	"encoding/binary"
 	"encoding/hex"
 	"strconv"
 
@@ -70,6 +71,19 @@ func (s *Server) ListenRTUForLinux(serialConfig *serial.Config, report func(err 
 	return err
 }
 
+// ListenRTU starts the Modbus server listening to a serial device.
+// For example:  err := s.ListenRTU(&serial.Config{Address: "/dev/ttyUSB0"})
+func (s *Server) ListenRTUForX(serialConfig *serial.Config, report func(err error)) (err error) {
+	port, err := serial.Open(serialConfig)
+	if err != nil {
+		log.Printf("failed to open %s: %v\n", serialConfig.Address, err)
+		return
+	}
+	s.ports = append(s.ports, port)
+	go s.acceptSerialRequestsX(port, report)
+	return err
+}
+
 func (s *Server) acceptSerialRequestsForLinux(port serial.Port, report func(err error)) {
 SkipFrameError:
 	for {
@@ -121,8 +135,9 @@ func (s *Server) ListenRTUForWindows(serialConfig *serial.Config, report func(er
 }
 
 var (
-	data  = make([]byte, 512)
-	total = 0
+	data               = make([]byte, 512)
+	total              = 0
+	registerDataLength = 0
 )
 
 func (s *Server) acceptSerialRequestsForWindows(port serial.Port, report func(err error)) {
@@ -166,6 +181,47 @@ SkipFrameError:
 			request := &Request{port, frame}
 
 			s.requestChan <- request
+		}
+	}
+}
+
+func (s *Server) acceptSerialRequestsX(port serial.Port, report func(err error)) {
+	for {
+		buffer := make([]byte, 512)
+
+		bytesRead, err := port.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("serial read error %v\n", err)
+			}
+			report(err)
+			return
+		}
+		log.Println("收到"+strconv.Itoa(bytesRead)+"字节:", hex.EncodeToString(buffer[:bytesRead]))
+		if bytesRead <= 2 {
+			data = buffer[:bytesRead]
+			total = total + bytesRead
+			continue
+		}
+		log.Println("数据收到长度", total)
+		data = append(data[:total], buffer[:bytesRead]...)
+		total = total + bytesRead
+		log.Println("数据帧内容", hex.EncodeToString(data))
+		if data[0] == 0x01 && data[1] == 0x10 && int(data[6])/int(binary.BigEndian.Uint16(data[4:6])) == 2 {
+			registerDataLength = int(data[6])
+			log.Println("校验第一包是否完整", hex.EncodeToString(data[:registerDataLength+9]))
+			if total == registerDataLength+9 {
+				// 包接收完整 开始解析
+				frame, err := NewRTUFrame(data)
+				if err != nil {
+					log.Printf("bad serial frame error %v\n", err)
+					log.Printf("Keep the RTU server running!!\n")
+				}
+				request := &Request{port, frame}
+				s.requestChan <- request
+				data = []byte{}
+				total = 0
+			}
 		}
 	}
 }
